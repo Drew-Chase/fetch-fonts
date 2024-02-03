0c3ed3c54d02aebeb486d0ff4fa904c0cf5642b4
Drew Chase <dcmanproductions@gmail.com>
2024-02-03 00:10:31 -0500

    Add Express webserver
    This commit adds a new Express.js webserver to the service. The web server serves static assets, handles POST requests, and sets up a route for downloading archives. Further, it also incorporates a cleanup utility for temporary files used during download.

---
+++ webserver.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {cleanup, generateOutputPath, getDataFromUrl} from "./fetch-fonts.js";
+
+const app = express();
+
+const args = process.argv.slice(2);
+const port = args[0] || 80;
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded());
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+// Generate and download the archive file
+app.get("/download", async (req, res) => {
+    const tempDirectory = generateOutputPath();
+    const url = req.query.url;
+    await getDataFromUrl(url, tempDirectory).then((archivePath) => {
+        res.status(200);
+        res.download(archivePath, "fonts.zip", (err) => {
+            if (err) {
+                console.log(err);
+            }
+            cleanup(archivePath, tempDirectory);
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
